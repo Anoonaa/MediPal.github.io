6d03ad94fbe3d5cf52a93bf4fafc5296d002508f
Anoona Sithole <anoonasithole0@gmail.com>
2024-06-12 21:48:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-
+$('.uis-form-icon').on('click', function(){
+	$(this).addClass('uis-active');
+	
+	$(this).siblings('input').addClass('uis-active');
+	$(this).siblings('input').focus();
+	
+	$(this).siblings('input').focusout(function(e){
+		$(this).removeClass('uis-active');
+		$(this).siblings('i').removeClass('uis-active');
+		$(this).val('');
+	})
+})
